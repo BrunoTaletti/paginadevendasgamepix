09a847dc3a8bbe1436dc003021d04c17d5532c53
Bruno Taletti <74149657+BrunoTaletti@users.noreply.github.com>
2022-04-28 04:00:50 -0300

    Comentários Facebook

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,16 @@
 // ID do seu botão de checkout
-var btnCheckout = "btn-checkout";
+var btnCheckout = [];
+
+// Quantidade de botões de checkout existentes
+btnCheckout[0] = "btn-checkout-1";
+btnCheckout[1] = "btn-checkout-2";
 
 // Tempo em ms que ocorre o pitch de vendas da sua VSL
 var pitchTime = 5000;
 
 // Regrinha que faz o botão aparecer quando o pitch de vendas chegar
 setTimeout(() => {
-    document.getElementById(btnCheckout).classList.remove("d-none")
+    for (var ctas in btnCheckout){
+        document.getElementById(btnCheckout[ctas]).classList.remove("d-none")
+    }
 }, pitchTime);
